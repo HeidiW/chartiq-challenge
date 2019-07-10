3f61582a79c36ff17d8d02e7e2c5ad90efdbf302
John Kreitlow <jkreitlow@jwplayer.com>
2019-07-10 16:44:59 -0400

    Reorganize a few things

--- script.js
+++ script.js
@@ -1,104 +1,81 @@
-//first, get data from CSV and return it as array
+// set up the CSV conversion function
+function getDataPointsFromCSV(csv) {
     const dataPoints = [];
-    //https://canvasjs.com/docs/charts/how-to/create-charts-from-csv/
-    function getDataPointsFromCSV(csv) {
-    let objectData = csvLines = points = [];
-    csvLines = csv.split(/[\r?\n|\r|\n]+/);
-        
-    for (var i = 0; i < csvLines.length; i++)
+    let points = [];
+    let csvLines = csv.split(/[\r?\n|\r|\n]+/);
+
+    for (var i = 1; i < csvLines.length; i++) {
         if (csvLines[i].length > 0) {
             points = csvLines[i].split(",");
-            dataPoints.push({ 
-                x: points[0], 
-                y: parseFloat(points[4]) 		
-	    });
-	 }
-    return dataPoints;
-   }
-   //then, get data and create the chart
-   $.get("https://s3.amazonaws.com/www.heidiwilliamsfoy.com/ibm.csv", function(data) {
-   const chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-      fontFamily: 'Roboto',
-      text: "Moving Average Closing Price of IBM",
-      padding: 10,
-    },
-    axisX: {
-      labelAngle: -30,
-    },
-    axisY: {
-      includeZero: false,
-      labelFormatter: e => `$${CanvasJS.formatNumber(e.value, "0.00")}`
-    },
-    data: [{
-      dataPoints,
-      markerSize: 10,
-      name: "Closing Price",
-      toolTipContent: "{x}</br>{name}: <strong>{y}</strong> USD",
-      type: "spline",
-      xValueFormatString: "MM-DD-YY",
-    }],
-    theme: "dark2"
-  });
-});
-// Function to calculate n-Day Simple moving average
-//https://jsfiddle.net/canvasjs/tq339Lm1/ works here
-function calculateMovingAverage(chart) {
-  var numOfDays = 20;
-  // return if there are insufficient dataPoints
-  if(chart.options.data[0].dataPoints.length <= numOfDays) return;
-  else {
-    // Add a new line series for  Moving Averages
-    chart.options.data.push({
-      type: "spline",
-      markerSize: 0,
-      name: "SMA",
-      yValueFormatString: "#,##0.00",
-      dataPoints: []
-    });
-    var total;
-    for(var i = numOfDays; i < chart.options.data[0].dataPoints.length; i++) {
-      total = 0;
-      for(var j = (i - numOfDays); j < i; j++) {
-        total += chart.options.data[0].dataPoints[j].y;
-      }
-      chart.options.data[1].dataPoints.push({
-        x: chart.options.data[0].dataPoints[i].x,
-        y: total / numOfDays
-      });
+            dataPoints.push({
+                x: new Date(points[0]),
+                y: parseFloat(points[4])
+            });
+        }
     }
-  }
-  calculateMovingAverage(chart);
-  chart.render();
+    return dataPoints;
 }
 
-  /* //calculate the moving average from the datapoints
-  // Function to calculate n-Day Simple moving average
-  function calculateMovingAverage(chart) {
-    const numOfDays = 20;
-    // return if there are insufficient dataPoints
-    if(chart.options.data[0].dataPoints.length <= numOfDays) return;
-    else {
-      // Add a new line series for  Moving Averages
-      chart.options.data.push({
-        type: "",
+
+// Take the data from the chart and calculate the 20-day moving average closing price
+function calculateMovingAverage(chart) {
+    let newPoints = [];
+    let numOfDays = 20;
+    const chartData = chart.options.data[0].dataPoints;
+
+    for(let i = numOfDays; i < chartData.length; i++) {
+        let total = 0;
+
+        for(let j = i - numOfDays; j < i; j++) {
+            total += chartData[j].y;
+        }
+
+        newPoints.push({
+            x: chartData[i].x,
+            y: total / numOfDays
+        });
+    }
+
+    // once all the data is calculated, we add a new line
+    chart.options.data.push({
+        type: "spline",
         markerSize: 0,
         name: "Moving Average of IBM Stock Over 20 Day Period",
-        yValueFormatString: "#,##0.00",
-        dataPoints: []
-      });
-      var total;
-      for(var i = numOfDays; i < chart.options.data[0].dataPoints.length; i++) {
-        total = 0;
-        for(var j = (i - numOfDays); j < i; j++) {
-          total += chart.options.data[0].dataPoints[j].y;
-        }
-        chart.options.data[1].dataPoints.push({
-          x: chart.options.data[0].dataPoints[i].x,
-          y: total / numOfDays
-        });
-      }
-    }
-  }
- */
+        dataPoints: newPoints,
+        toolTipContent: "{x}</br>{name}: <strong>{y}</strong> USD"
+    });
+}
 
+
+// First, get the csv, then run the function as a callback
+$.get("./ibm.csv", function(response) {
+    const chart = new CanvasJS.Chart("chartContainer", {
+        title: {
+            fontFamily: 'Roboto',
+            text: "Moving Average Closing Price of IBM",
+            padding: 10,
+        },
+        axisX: {
+            labelAngle: -30,
+        },
+        axisY: {
+            includeZero: false,
+            labelFormatter: e => `$${CanvasJS.formatNumber(e.value, "0.00")}`
+        },
+        data: [{
+            dataPoints: getDataPointsFromCSV(response),
+            markerSize: 5,
+            name: "Closing Price",
+            toolTipContent: "{x}</br>{name}: <strong>{y}</strong> USD",
+            type: "spline",
+            xValueFormatString: "MM-DD-YY",
+        }],
+        theme: "dark2"
+    });
+
+    // after setting up the chart object, add the second line for the moving average
+    calculateMovingAverage(chart);
+
+    // when everything's done, render the chart
+    chart.render();
+});
