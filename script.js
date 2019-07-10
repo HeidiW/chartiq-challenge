edbcfa4e4e4531585e50f70388a44c0f69286dae
Heidi Williams-Foy <foywilliams@gmail.com>
2019-07-10 14:42:39 -0400

    rearranged functions but still not working

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //first, get data from CSV and return it as array
     const dataPoints = [];
-    $.get("https://s3.amazonaws.com/www.heidiwilliamsfoy.com/ibm.csv", function(data) {
     function getDataPointsFromCSV(csv) {
     let objectData = csvLines = points = [];
     csvLines = csv.split(/[\r?\n|\r|\n]+/);
@@ -15,33 +14,63 @@
 	 }
     return dataPoints;
    }
-   //then, create the chart
+   //then, get data and create the chart
+   $.get("https://s3.amazonaws.com/www.heidiwilliamsfoy.com/ibm.csv", function(data) {
    const chart = new CanvasJS.Chart("chartContainer", {
     title: {
-      text: "Column Chart with 20 Day Simple Moving Average"
-    },
-    toolTip: {
-      shared: true
-    },
-    axisY: {
-      includeZero: false
+      fontFamily: 'Roboto',
+      text: "Moving Average Closing Price of IBM",
+      padding: 10,
     },
     axisX: {
-      valueFormatString: "DD-MMM-YY"
+      labelAngle: -30,
+    },
+    axisY: {
+      includeZero: false,
+      labelFormatter: e => `$${CanvasJS.formatNumber(e.value, "0.00")}`
     },
     data: [{
+      dataPoints,
+      markerSize: 10,
+      name: "Closing Price",
+      toolTipContent: "{x}</br>{name}: <strong>{y}</strong> USD",
       type: "spline",
-      xValueFormatString: "DD(DDD) MMM YY(YYY)",
-      yValueFormatString: "#,##0.00",
-      name: "Stock Price",
-      dataPoints
-    }]
+      xValueFormatString: "MM-DD-YY",
+    }],
+    theme: "dark2"
   });
-  
+});
+// Function to calculate n-Day Simple moving average
+function calculateMovingAverage(chart) {
+  var numOfDays = 20;
+  // return if there are insufficient dataPoints
+  if(chart.options.data[0].dataPoints.length <= numOfDays) return;
+  else {
+    // Add a new line series for  Moving Averages
+    chart.options.data.push({
+      type: "spline",
+      markerSize: 0,
+      name: "SMA",
+      yValueFormatString: "#,##0.00",
+      dataPoints: []
+    });
+    var total;
+    for(var i = numOfDays; i < chart.options.data[0].dataPoints.length; i++) {
+      total = 0;
+      for(var j = (i - numOfDays); j < i; j++) {
+        total += chart.options.data[0].dataPoints[j].y;
+      }
+      chart.options.data[1].dataPoints.push({
+        x: chart.options.data[0].dataPoints[i].x,
+        y: total / numOfDays
+      });
+    }
+  }
   calculateMovingAverage(chart);
   chart.render();
+}
 
-  //calculate the moving average from the datapoints
+  /* //calculate the moving average from the datapoints
   // Function to calculate n-Day Simple moving average
   function calculateMovingAverage(chart) {
     const numOfDays = 20;
@@ -69,3 +98,5 @@
       }
     }
   }
+ */
+
